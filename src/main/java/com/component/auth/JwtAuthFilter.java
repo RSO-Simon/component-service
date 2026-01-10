a2a4a22d00fbe028f2c72e887a76623b49bac64a
Doeoeo <sc3544@student.uni-lj.si>
2026-01-10 17:01:47 +0100

    auth health fix

--- src/main/java/com/component/auth/JwtAuthFilter.java
+++ src/main/java/com/component/auth/JwtAuthFilter.java
@@ -22,7 +22,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String p = request.getRequestURI();
         return p.startsWith("/actuator")
-                | p.startsWith("/api/component/actuator")
+                || p.startsWith("/api/component/actuator")
                 || p.startsWith("/swagger")
                 || p.startsWith("/v3/api-docs");
     }
